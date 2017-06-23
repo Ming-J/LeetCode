c5cfd3a5fd1ccfbeec7fcaec9fdbd9982af05a3a
U-UK\minwu <minwu@uk166672.uk.deloitte.com>
2017-06-23 17:27:27 +0100

    Finished problem 504 Base of 7

--- 504_Base_7.cpp
+++ 504_Base_7.cpp
@@ -1,27 +1,47 @@
+/*****************************************************************************
+Given an integer, return its base 7 string representation.
+*
+*Example 1:
+*  Input: 100
+*  Output: "202"
+*Example 2:
+*  Input: -7
+*  Output: "-10"
+*Note: The input will be in range of [-1e7, 1e7].
+******************************************************************************/
 #include <iostream>
 #include <string>
 #include <sstream>
 using namespace std;
 
+
 class Solution{
 public:
   string converToBase(int num){
-    int power=1;
+    bool sign=false;
     int value=7;
     int reminder=0;
     string ans="";
+    if(num==0){
+      ans="0";
+      return ans;
+    }
+    if(num<0){
+      num=num*-1;
+      sign=true;
+    }
     while(num>0){
       value=num/7;
       reminder=num%7;
-      cout<<value<<" "<<reminder<<endl;
       num=value;
       stringstream ss;
       ss<<reminder;
       ans.insert(0,ss.str());
-      //oss<<reminder<<ans;
       cout<<ans<<endl;
     }
-    //cout<<oss.str()<<num<<endl;
+    if(sign==true){
+      ans.insert(0,"-");
+    }
     return ans;
   }
 };
