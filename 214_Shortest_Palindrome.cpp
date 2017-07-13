4f6ea004e26b79d181eadf7a928d4787ca1c7673
mw12g <mingjouwu@gmail.com>
2017-07-13 18:43:29 -0400

    Completed 214. IMplement partial table for KMP algorithm

--- 214_Shortest_Palindrome.cpp
+++ 214_Shortest_Palindrome.cpp
@@ -1,3 +1,29 @@
+/*================================================================
+
+Given a string S, you are allowed to convert it to a palindrome by
+adding characters in front of it. Find and return the shortest
+palindrome you can find by performing this transformation.
+
+For example:
+
+Given "aacecaaa", return "aaacecaaa".
+
+Given "abcd", return "dcbabcd".
+
+logic for this problem is say we have:
+-Sting S
+-Added string in front is P
+
+   PS= PQP'  where QP'=S 
+So if P and P' is palindrome, Q is also Palindrome
+
+We would like P as short as possible, so Q has to be as long as
+possible.
+
+S=QP apprend the reverse SS'=QPP'Q' We know Q is a Palindrome so
+SS'=QPP'Q now what we want to find the longest Q where Q is the prefix
+of SS' also a suffix of SS'.
+ =================================================================*/
 #include <iostream>
 #include <vector>
 using namespace std;
@@ -8,18 +34,18 @@ public:
     if(s.size()==0){
       return "";
     }
-    string rev=s+reversestring(s);
-    vector<int> partialtable=kmpPrefix(rev);
+    string reverseS=reversestring(s);
+    string rev=s+"#"+reverseS;
+    vector<int> partialtable=kmptable(rev);
     int revsize=rev.size();
     int ptsize=partialtable.size();
     int suffixindex=partialtable[ptsize-1];
     if(suffixindex==revsize){
       return s;
     }
-    
-    string sufix=rev.substr(revsize-suffixindex,suffixindex);
-    string prefix=reversestring(s).substr(0,reversestring(s).size()-sufix.size());
-    cout<<rev<<" "<<sufix<<" "<<prefix<<endl;
+    //string sufix=rev.substr(revsize-suffixindex,suffixindex);
+    string prefix=reverseS.substr(0,reverseS.size()-suffixindex);
+    //cout<<rev<<" "<<sufix<<" "<<prefix<<endl;
     return prefix+s;
   }
   string reversestring(string s){
@@ -29,6 +55,28 @@ public:
     }
     return reverse;
   }
+
+  vector<int>kmptable(string s){
+    vector<int> prefix(s.size(),0);
+    int i=1;
+    int j=0;
+    while(i<prefix.size()){
+      if(s[i]==s[j]){
+	prefix[i]=j+1;
+	i=i+1;
+	j=j+1;
+      }else{
+	if(j!=0){
+	  j=prefix[j-1];
+	}else{
+	  prefix[i]=0;
+	  i=i+1;
+	}
+      }
+    }
+    return prefix;
+  }
+  
   vector<int> kmpPrefix(string s){
     vector<int> prefixtable(s.size(),0);
     int index=1;
@@ -43,9 +91,6 @@ public:
       }
       prefixtable[index]=sindex;
     }
-    for(int i=0;i<prefixtable.size();i++){
-      cout<<s[i]<<" "<<prefixtable[i]<<endl;
-    }
     return prefixtable;
   }
 };
@@ -53,6 +98,7 @@ public:
 int main(){
   Solution test;
   //test.shortestPalindrome("ABAB");
-  //test.kmpPrefix(test.shortestPalindrome("ABAB"));
-  cout<<test.shortestPalindrome("aaaaa")<<endl;
+  test.kmpPrefix("ABAB");
+  test.kmptable("ABAB");
+  cout<<test.shortestPalindrome()<<end;
 }
