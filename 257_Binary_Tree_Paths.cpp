5672bb526ed47776bf802e486e6b11fe2462fe94
U-UK\minwu <minwu@uk166672.uk.deloitte.com>
2017-08-24 18:13:59 +0100

    a

---
+++ 257_Binary_Tree_Paths.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+struct TreeNode{
+  int val;
+  TreeNode* left;
+  TreeNode* right;
+}
+class Solution{
+public:
+  vector<string> binaryTreePaths(TreeNode* root){
+    
+  }
+};
