b8d9309af2ac0c0651243223cac44474ae61acd3
U-UK\minwu <minwu@uk166672.uk.deloitte.com>
2017-09-22 17:22:23 +0100

    a

--- 70_Climbing_Stairs.cpp
+++ 70_Climbing_Stairs.cpp
@@ -7,15 +7,21 @@ private:
   vector<int> storage;
 public:
   int climbStairs(int n){
-    storage(n,0);
     if(n==1){
       return 1;
     }
     if(n==2){
       return 2;
     }
-    int ways=0;
-    ways=climbStairs(n-1)+climbStairs(n-2);
+    int ways;
+    int dpone=1;
+    int dptwo=2;
+    for(int i=3;i<=n;i++){
+      ways=dpone+dptwo;
+      dptwo=dpone;
+      dpone=ways;
+    }
+    //ways=climbStairs(n-1)+climbStairs(n-2);
     return ways;
   }
 };
