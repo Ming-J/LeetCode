dcb93f50097b53d5ea1eff059a689e93a00680f3
U-UK\minwu <minwu@uk166672.uk.deloitte.com>
2017-10-16 17:51:21 +0100

    a

--- 200_Number_Of_Islands.cpp
+++ 200_Number_Of_Islands.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 #include <vector>
 using namespace std;
-
 class Solution {
 public:
     int numIslands(vector<vector<char>>& grid) {
+      if(grid.size()==0){
+	return 0;
+      }
       int numberOfIsland=0;
       vector< vector<bool> > truth(grid.size(),
 				   vector<bool> (grid[0].size(),false));
@@ -19,18 +21,38 @@ public:
       return numberOfIsland;
     }
   void helper(vector<vector<char> >& grid, vector<vector<bool>> &tru, int row, int col){
+    if(row<0||row>=grid.size()||col<0||col>=gird[0].size()){
+      return;
+    }
     if(tru[row][col]==true){
       return;
     }else{
       if(row>=0&&row<grid.size()&&col>=0&&col<grid[row].size()){
-	tru[row][col]==true;
+	//print(tru);
 	if(grid[row][col]=='1'){
+	  tru[row][col]=true;
 	  helper(grid,tru,row+1,col);
 	  helper(grid,tru,row-1,col);
 	  helper(grid,tru,row,col+1);
 	  helper(grid,tru,row,col-1);
+	}else{
+	  return;
 	}
       }
+    }
+  }
+
+
+  void print(vector<vector<bool>> tru){
+    for(int i=0;i<tru.size();i++){
+      for(int j=0;j<tru[i].size();j++){
+	if(tru[i][j]==true){
+	  cout<<'T';
+	}else{
+	  cout<<'F';
+	}
+      }
+      cout<<endl;
     }
   }
 };
