f32a32c192c95e6f0ca97b35ca13cee04f2f32a6
mw12g <mingjouwu@gmail.com>
2017-10-25 00:11:07 +0100

    a

--- 60_Permutation_Sequence.cpp
+++ 60_Permutation_Sequence.cpp
@@ -1,16 +1,49 @@
 #include <iostream>
+#include <algorithm>
 using namespace std;
 
 class Solution{
-  string getPermutation(int n, int k){
-    
-  }
-
-  void helper(int n, int start, int k, string &s){
-    for(size_t i=start;i<=n;i++){
-      
-
+private:
+  int calfactorial(int k){
+    int n=1;
+    for(int i=0;i<k;i++){
+      n*=i+1;
     }
+    return n;
   }
-  
+public:
+  string getPermutation(int n, int k){
+    string s="";
+    string ans="";
+    for(int i=0;i<n;i++){
+      s+=to_string(i+1);
+    }
+    cout<<s<<endl;
+    while(ans.size()!=n){
+      int factor=calfactorial(s.size()-1);
+      cout<<"k:"<<k<<endl;
+      int permu=calfactorial(s.size());
+      //int mod=k%permu;
+      int rem=k/factor;
+      k%=factor;
+      if(k==0){
+	rem--;
+	k=factor;
+      }
+      cout<<"f:"<<factor<<endl;
+      cout<<"rem:"<<rem<<endl;
+      char c=s[rem];
+      s.erase(rem,1);
+      ans+=c;
+      cout<<ans<<endl;
+    }
+    //helper(s,0,k,ans);
+    return ans;
+  }
 };
+
+int main(){
+  Solution test;
+  cout<<test.getPermutation(3,4)<<"<-----"<<endl;;
+  return 0;
+}
