1104c9691333b5fc52a38c8d384d4e7e603ead5b
mw12g <mingjouwu@gmail.com>
2017-10-24 08:39:11 +0100

    a

---
+++ 412_Fizz_Buzz.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution{
+public:
+  vector<string> fizzBuzz(int n){
+    vector<string> a;
+    for(int i=0;i<n;i++){
+      if((i+1)%15==0||(i+1)%5==0||(i+1)%3==0){
+	if((i+1)%15==0){
+	  a.push_back("FizzBuzz");
+	}else if((i+1)%5==0){
+	  a.push_back("Buzz");
+	}else{
+	  a.push_back("Fizz");
+	}
+      }else{
+	a.push_back(to_string(i+1));
+      }
+    }
+    return a;
+  }
+};
