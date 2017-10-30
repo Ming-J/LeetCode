667e9ee951a4bdfc190f04b6022c60d687a73795
mw12g <mingjouwu@gmail.com>
2017-10-30 22:23:09 +0000

    a

--- 463_Island_Perimeter.cpp
+++ 463_Island_Perimeter.cpp
@@ -1,19 +1,17 @@
 #include <iostream>
 #include <vector>
 using namespace std;
-
 class Solution {
 public:
     int islandPerimeter(vector<vector<int> >& grid) {
       if(grid.size()==0||grid[0].size()==0){
 	return 0;
       }
-      //vector<bool> vr(grid[0].size(),false);
       vector< vector<bool> > visited(grid.size(),
 				     vector<bool>(grid[0].size(),false));
       int primeter=0;
       for(size_t i=0;i<grid.size();i++){
-	for(size_t j=0;grid[j].size();j++){
+	for(size_t j=0;j<grid[i].size();j++){
 	  if(grid[i][j]){
 	    dsf(grid,primeter,i,j,visited);
 	  }
@@ -21,7 +19,6 @@ public:
       }
       return primeter;
     }
-
   void dsf(vector<vector<int> >& grid, int &primeter, int row, int col,
 	   vector<vector<bool> > &visited){
     int rowUB=grid.size();
@@ -38,10 +35,10 @@ public:
       if(col==0){primeter++;}
       if(row==grid.size()-1){primeter++;}
       if(col==grid[row].size()-1){primeter++;}
-      if(grid[row+1][col]==0){primeter++;}
-      if(grid[row-1][col]==0){primeter++;}
-      if(grid[row][col+1]==0){primeter++;}
-      if(grid[row][col-1]==0){primeter++;}
+      if(row+1<rowUB&&grid[row+1][col]==0){primeter++;}
+      if(row-1>=0&&grid[row-1][col]==0){primeter++;}
+      if(col+1<colUB&&grid[row][col+1]==0){primeter++;}
+      if(col-1>=0&&grid[row][col-1]==0){primeter++;}
       dsf(grid,primeter,row+1,col,visited);
       dsf(grid,primeter,row-1,col,visited);
       dsf(grid,primeter,row,col+1,visited);
