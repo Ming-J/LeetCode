5b7468f78433ae77260a9e9665724199b1b1f534
U-UK\minwu <minwu@uk166672.uk.deloitte.com>
2017-11-16 18:08:45 +0000

    a

---
+++ 594_Longest_Harmonious_Subsequence.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+using namespace std;
+
+class Solution{
+public:
+  int findLHS(vector<int>& nums){
+    unordered_map<int,int> hash;
+    for(int i=0;i<nums.size();i++){
+      hash[nums]++;
+    }
+    int 
+    for(auto i=0;i!=hash.end();i++){
+
+    }
+    return 0;
+  }
+};
