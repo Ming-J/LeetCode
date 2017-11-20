d288124bc708d32032c094d54774a748525a8c5f
U-UK\minwu <minwu@uk166672.uk.deloitte.com>
2017-11-20 18:18:17 +0000

    a

---
+++ 6_ZigZag_Conversion.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution{
+public:
+  string convert(string s,int numRows){
+    vector<vector<string> > zigzag (numRows,vector<string>); 
+    return "";
+  }
+};
