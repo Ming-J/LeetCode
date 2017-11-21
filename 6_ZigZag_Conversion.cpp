feb23777a0b0b509357cc6be0df58ac899590047
mw12g <mingjouwu@gmail.com>
2017-11-21 00:08:10 +0000

    a

--- 6_ZigZag_Conversion.cpp
+++ 6_ZigZag_Conversion.cpp
@@ -5,7 +5,34 @@ using namespace std;
 class Solution{
 public:
   string convert(string s,int numRows){
-    vector<vector<string> > zigzag (numRows,vector<string>); 
-    return "";
+    if(numRows==1){return s;}
+    vector<vector<char> > zigzag (numRows,vector<char> ());
+    bool downflag=true;
+    int rowIndex=0;
+    for(int i=0;i<s.size();i++){
+      zigzag[rowIndex].push_back(s[i]);
+      if(downflag){
+	if(rowIndex==numRows-1){
+	  downflag=false;
+	  rowIndex--;
+	}else{
+	  rowIndex++;
+	}
+      }else{
+	if(rowIndex==0){
+	  downflag=true;
+	  rowIndex++;
+	}else{
+	  rowIndex--;
+	}
+      }
+    }
+    string ans="";
+    for(int i=0;i<zigzag.size();i++){
+      for(int j=0;j<zigzag[i].size();j++){
+	ans+=zigzag[i][j];
+      }
+    }
+    return ans;
   }
 };
