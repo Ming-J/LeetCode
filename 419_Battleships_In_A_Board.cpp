8fb2bc00496b19d8384838f50646a3dfba07443f
U-UK\minwu <minwu@uk166672.uk.deloitte.com>
2017-11-28 17:30:57 +0000

    a

--- 419_Battleships_In_A_Board.cpp
+++ 419_Battleships_In_A_Board.cpp
@@ -7,14 +7,14 @@ public:
   int countBattleships(vector<vector<char> >& board){
     int count=0;
     for(int i=0;i<board.size();i++){
-      for(int j=0;j<board[i].size();i++){
+      for(int j=0;j<board[i].size();j++){
 	if(board[i][j]=='X'){
 	  if(i==0&&j==0){
 	    count++;
-	  }else if(i==0&&board[i][j-1]!='X'){
-	    count++;
-	  }else if(j==0&&board[i-1][j]!='X'){
-	    count++;
+	  }else if(i==0){
+	    if(board[i][j-1]!='X'){count++;}
+	  }else if(j==0){
+	    if(board[i-1][j]!='X'){count++;}
 	  }else if(board[i-1][j]!='X'&&board[i][j-1]!='X'){
 	    count++;
 	  }
