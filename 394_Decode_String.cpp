3f80c4e2d1f9576a1687cc9b6c804c3ab8143933
U-UK\minwu <minwu@uk166672.uk.deloitte.com>
2017-11-29 17:26:59 +0000

    a

---
+++ 394_Decode_String.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+class Solution{
+public:
+  string decodeString(string s){
+    string ans="";
+    stack<int> numberDeck;
+    stack<string> letterDeck;
+    string current="";
+    for(int i=0;i<s.size();i++){
+      if(s[i]=='['){
+	string num="";
+	for(int k=i-1;i>=0&&s[k]-'0'<10;k--){
+	  num=s[k]+num;
+	  current.pop_back();
+	}
+	numberDeck.push(stoi(num));
+	letterDeck.push(current);
+	current.erase();
+      }else if(s[i]==']'){
+	int rep=numberDeck.top();
+	numberDeck.pop();
+	string t=letterDeck.top();
+	letterDeck.pop();
+	for(int j=0;j<rep;j++){
+	  t+=current;
+	}
+	current=t;
+      }else{
+	current+=s[i];
+      }      
+    }
+    return ans+current;
+  }
+};
