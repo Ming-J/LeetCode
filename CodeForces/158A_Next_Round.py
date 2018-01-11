38ce06c9ae567c6d76e405e42995a9757aa86758
U-UK\minwu <minwu@uk166672.uk.deloitte.com>
2018-01-11 17:49:35 +0000

    a

--- CodeForces/158A_Next_Round.py
+++ CodeForces/158A_Next_Round.py
@@ -1,2 +1,18 @@
+from sys import stdin, stdout
+
 def main():
-    print("hello world");
+    n,k = map(int,stdin.readline().split())
+    arr = [int(x) for x in stdin.readline().split()]
+
+    x=0
+
+    for i,val in enumerate(arr):
+        if ( (i<k and val >0) or (arr[k-1]==val and val>0 )):
+            x+=1
+    
+    print x
+
+if __name__=="__main__":
+    main()
+
+    
