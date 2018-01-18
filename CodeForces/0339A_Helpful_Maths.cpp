1eaa1125f02d6e9b22c95b93785ab2a46a3d0589
mw12g <mingjouwu@gmail.com>
2018-01-18 08:47:26 +0000

    a

--- CodeForces/0339A_Helpful_Maths.cpp
+++ CodeForces/0339A_Helpful_Maths.cpp
@@ -3,20 +3,32 @@
 using namespace std;
 
 int main(){
-  string fmu = "3+2+1";
+  string fmu = "3+2+1+2+3";
+  cin>>fmu;
   char c_arr[fmu.size()+1];
   //c_arr = strdup( fmu.c_str() );
-  strcpy(c_arr,fmu.c_str());
-  for( int i =0 ; i< fmu.size()+1; i++){
-    cout<<c_arr[i];
-  }
+
+  //making an array copy
+  strcpy(c_arr,fmu.c_str());;
+
+
   //char *token = strtok(const_cast<char *>(fmu.c_str()),"+");
+  //using strtok will modify the original string with "@"
   char *token = strtok(&fmu[0],"+");
   int nArr[3]={0};
   while(token != NULL){
-    cout<<token<<endl;
-    nArr[token-'0']++;
+    nArr[atoi(token)-1]++;
     token = strtok(NULL, "+");
+  }
+  string res;
+  for(int i=0;i < 3;i++){
+    for(int j=0;j<nArr[i];j++){
+      if(res.empty()){
+	res += to_string(i+1);
+      }else{
+	res += '+'+to_string(i+1);
+      }
     }
-  cout<<fmu<<endl;
+  }
+  cout<<res<<endl;
 }
