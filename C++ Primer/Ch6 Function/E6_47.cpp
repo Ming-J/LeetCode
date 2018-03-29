3751ded494135962d7c5b370a110a934aaa58ad9
U-UK\minwu <minwu@uk166672.uk.deloitte.com>
2018-03-29 17:35:57 +0100

    a

--- C++ Primer/Ch6 Function/E6_47.cpp
+++ C++ Primer/Ch6 Function/E6_47.cpp
@@ -1,13 +1,21 @@
-
 #include <iostream>
 #include <vector>
 using namespace std;
 
+/*
+  To turn off debugging, uncomment the following line, or compile the
+  program with '-D NDEBUG' switch
+*/
+
 void printvectorRec(vector<int>::iterator first, vector<int>::iterator last){
+  #ifndef NDEBUG
+  cerr<<__FILE__<<' '<<__DATE__<<' '<<__TIME__<<' '<<__func__<<endl;
+  #endif
   if( first != last){
     cout<<*first<<endl;
     printvectorRec(++first,last);
   }
+  
 }
 
 int main(){
