a53f9dbe13f1815c5c6d54d4442df6899c5bfa05
mw12g <mingjouwu@gmail.com>
2018-04-10 00:17:46 +0100

    a

--- 438_Find_All_Anagrams_In_A_String.cpp
+++ 438_Find_All_Anagrams_In_A_String.cpp
@@ -6,13 +6,24 @@ class Solution {
 public:
   vector<int> findAnagrams(string s, string p) {
     vector<int> ans;
-    int *ca = getAmagrams(p);
-    for(int& a : ca){
-      cout<<a<<endl;
+    if(p.size() > s.size()) return ans;
+    vector<int> pArray = getAma(p);
+    vector<int> subArray = getAma(s.substr(0,p.size()));
+    if(pArray == subArray) ans.push_back(0);
+    for(size_t i = 1; i < s.size() - p.size() + 1; ++i){
+      --subArray[s[i-1]-'a'];
+      ++subArray[s[i+p.size()-1]-'a'];
+      if(pArray == subArray) ans.push_back(i);
     }
     return ans;
   }
 private:
+  vector<int> getAma(string p){
+    vector<int> ca(26,0);
+    for(char c : p) ++ca[c-'a'];
+    return ca;
+  }
+  
   int* getAmagrams(string p){
     static int carray[26] = {0};
     for(char c : p){
@@ -24,5 +35,8 @@ private:
 
 int main(){
   Solution test;
-  vector<int> res = test.findAnagrams("adsfsdfa","ab");
+  vector<int> res = test.findAnagrams("cbaebabacd","abc");
+  for(auto re : res){
+    cout<<re<<endl;
+  }
 }
