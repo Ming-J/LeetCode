4fcd07bc9d65a3ffcc2ee3fabdfa39f55d635b44
U-UK\minwu <minwu@uk166672.uk.deloitte.com>
2018-05-16 17:08:58 +0100

    a

--- 042_Trapping_Rain_Water.cpp
+++ 042_Trapping_Rain_Water.cpp
@@ -6,7 +6,24 @@ using namespace std;
 class Solution {
 public:
   int trap(vector<int>& height) {
-    stack<int> s;
+    stack< pair<int,int> > s;
+    int volume = 0;
+    for(size_t i = 0; i < height.size(); ++i){
+      int prevHeight = 0;
+      while(!s.empty()){
+	int bar = s.top().first;
+	int pos = s.top().second;
+	volume += (min(bar,height[i]) - prevHeight) * (i - pos - 1);
+	prevHeight = bar;
+	if(height[i] < bar){
+	  break;
+	}else{
+	  s.pop();
+	}
+      }
+      s.push(make_pair(height[i],i));
+    }
+    cout<<"stack|"<<volume<<endl;
     vector<int> maxLeft(height.size(),0);
     vector<int> maxRight(height.size(),0);
     for(int i = 1; i < height.size(); ++i){
@@ -14,7 +31,15 @@ public:
       maxRight[height.size() - 1 - i] =
 	max(maxRight[height.size() - i], height[height.size() - i]);
     }
-
+    int sum = 0;
+    for(size_t i = 0; i < height.size(); ++i){
+      int minheight = min(maxLeft[i],maxRight[i]);
+      if(minheight > height[i]){
+	cout<<i<<' '<<minheight-height[i]<<endl;
+	sum += minheight - height[i]; 
+      }
+    }
+    cout<<"arr|"<<sum<<endl;
     for(int i : height){
       cout<<i<<' ';
     }
