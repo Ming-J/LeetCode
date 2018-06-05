b6933f8ff2c67b22758835fca29d17406409bdb6
apple <apple@applematoMacBook-Pro-2.local>
2018-06-05 08:46:05 +0100

    a

--- CodeForces/0621A_Wet_Shark_And_Odd_And_Even.cpp
+++ CodeForces/0621A_Wet_Shark_And_Odd_And_Even.cpp
@@ -5,19 +5,22 @@ using namespace std;
 
 int main(){
   int n;
+  cin>>n;
   vector<int> nums(n);
-  long sum = 0;
+  long long int sum = 0;
   for(auto& i : nums){
     cin>>i;
     sum += i;
   }
+  nums.push_back(0);
   sort(nums.begin(),nums.end());
   int lo = 0;
   while(lo<nums.size()){
-    if((sum-num[lo])%2==0){
-      cout<<sum-num[lo]<<endl;
+    if((sum-nums[lo])%2==0){
+      cout<<sum-nums[lo]<<endl;
       return 0;
     }
+    ++lo;
   }
   cout<<0<<endl;
 }
