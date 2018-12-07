8732ea75b453d570b75ce1dde4f019ef8ed42a84
U-UK\minwu <minwu@UK166672.uk.deloitte.com>
2018-12-07 18:06:41 +0000

    a

---
+++ CodeForces/0276C_Little_Girl_And_Maximum_Sum.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class SUM{
+public:
+  SUM(int n){
+    called(n);
+  }
+
+  
+  
+private:
+  vector<int> called;
+};
+
+
+int main(){
+  cout<<"hello"<<endl;
+}
